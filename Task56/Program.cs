3c2ead7c2b5ee9490bc49df3ee1704d7592b8364
Yaroslav63 <era6300@mail.ru>
2022-04-11 12:15:30 +0300

    add Task 56

---
+++ Task56/Program.cs
@@ -0,0 +1,40 @@
+﻿int n = 3,
+    m = 8;
+int[,] array = new int[n, m];
+int[] sum = new int[n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        sum[i] = sum[i] + array[i, j];
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{sum[i]} ");
+}
+Console.WriteLine();
+int min = sum[0];
+int strMin = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (sum[i] < min)
+        {
+            min = sum[i];
+            strMin = i + 1;
+        }
+    }
+}
+Console.Write($"номер строки {strMin} с наименьшей суммой элементов {min}");
